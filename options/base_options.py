3b279602a957c68eb8d4808a216340e247088cd6
Caner Hazirbas <hazirbas@cs.tum.edu>
2018-01-16 11:03:54 +0100

    typo fixed.

--- options/base_options.py
+++ options/base_options.py
@@ -38,7 +38,7 @@ class BaseOptions():
         self.parser.add_argument('--no_dropout', action='store_true', help='no dropout for the generator')
         self.parser.add_argument('--max_dataset_size', type=int, default=float("inf"), help='Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
         self.parser.add_argument('--resize_or_crop', type=str, default='scale_width_and_crop', help='scaling and cropping of images at load time [resize_and_crop|crop|scale_width|scale_width_and_crop]')
-        self.parser.add_argument('--no_flip', action='store_true', default=True help='if specified, do not flip the images for data augmentation')
+        self.parser.add_argument('--no_flip', action='store_true', default=True, help='if specified, do not flip the images for data augmentation')
         self.parser.add_argument('--init_type', type=str, default='normal', help='network initialization [normal|xavier|kaiming|orthogonal]')
 
         self.initialized = True
