3cf6ffe1929de8dbed95a1c41113adb6b39d2518
Caner Hazirbas <hazirbas@cs.tum.edu>
2018-02-01 16:58:08 +0100

    renamed no_html to use_html

--- util/visualizer.py
+++ util/visualizer.py
@@ -10,7 +10,7 @@ class Visualizer():
     def __init__(self, opt):
         # self.opt = opt
         self.display_id = opt.display_id
-        self.use_html = opt.isTrain and not opt.no_html
+        self.use_html = opt.isTrain and opt.use_html
         self.win_size = opt.display_winsize
         self.name = opt.name
         self.opt = opt
